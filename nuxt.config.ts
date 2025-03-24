619f82e863e4453f29db79e86dcee03806e708f9
Higor Moroni <moroni.higor@gmail.com>
2025-03-23 23:16:23 -0300

    feat: add new modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/image']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/icon'
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  }
 })
